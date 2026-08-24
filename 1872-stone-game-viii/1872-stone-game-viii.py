26bb8c57db5ae1619bb1b46012910f043d1481b1
varun <varunsai.1028@gmail.com>
2026-08-24 13:33:17 +0530

    Time: 679 ms (57.99%), Space: 33 MB (63.91%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n = len(stones)
+        
+
+        for i in range(1, n):
+            stones[i] += stones[i - 1]
+            
+
+        dp = stones[-1]
+        
+
+        for i in range(n - 2, 0, -1):
+            dp = max(dp, stones[i] - dp)
+            
+        return dp
